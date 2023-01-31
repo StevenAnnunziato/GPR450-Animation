767f3be008c3f76e5030067cd50cde68569e1230
Steven Annunziato <saannunziato@gmail.com>
2023-01-31 11:09:10 -0500

    Moved a few functions from the .c file to the .inl file.

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_inl/a3_KeyframeAnimation.inl
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_inl/a3_KeyframeAnimation.inl
@@ -32,13 +32,36 @@
 // calculate clip duration as sum of keyframes' durations
 inline a3i32 a3clipCalculateDuration(a3_Clip* clip)
 {
-	return -1;
+	// find the sum of all keyframe durations
+	a3real sum = 0;
+	for (int i = clip->firstKeyframeIndex; i <= clip->lastKeyframeIndex; i++)
+	{
+		sum += clip->keyframePool->keyframe[i].duration;
+	}
+
+	clip->duration = sum;
+	clip->inverseDuration = 1.0f / sum;
+
+	return 0;
 }
 
 // calculate keyframes' durations by distributing clip's duration
 inline a3i32 a3clipDistributeDuration(a3_Clip* clip, const a3real newClipDuration)
 {
-	return -1;
+	// set the new clip duration
+	clip->duration = newClipDuration;
+	clip->inverseDuration = 1.0f / newClipDuration;
+
+	// assign a uniform time to all keyframes
+	a3real keyframeDuration = newClipDuration / clip->keyframeCount;
+	a3real invDuration = 1.0f / keyframeDuration;
+	for (int i = clip->firstKeyframeIndex; i <= clip->lastKeyframeIndex; i++)
+	{
+		clip->keyframePool->keyframe[i].duration = keyframeDuration;
+		clip->keyframePool->keyframe[i].inverseDuration = invDuration;
+	}
+
+	return 0;
 }
 
 
--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_KeyframeAnimation.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_KeyframeAnimation.c
@@ -127,39 +127,4 @@ a3i32 a3clipGetIndexInPool(const a3_ClipPool* clipPool, const a3byte clipName[a3
 	return -1;
 }
 
-// calculate clip duration as sum of keyframes' durations
-a3i32 a3clipCalculateDuration(a3_Clip* clip)
-{
-	// find the sum of all keyframe durations
-	a3real sum = 0;
-	for (int i = clip->firstKeyframeIndex; i <= clip->lastKeyframeIndex; i++)
-	{
-		sum += clip->keyframePool->keyframe[i].duration;
-	}
-
-	clip->duration = sum;
-	clip->inverseDuration = 1.0f / sum;
-
-	return 0;
-}
-
-// calculate keyframes' durations by distributing clip's duration
-a3i32 a3clipDistributeDuration(a3_Clip* clip, const a3real newClipDuration)
-{
-	// set the new clip duration
-	clip->duration = newClipDuration;
-	clip->inverseDuration = 1.0f / newClipDuration;
-
-	// assign a uniform time to all keyframes
-	a3real keyframeDuration = newClipDuration / clip->keyframeCount;
-	a3real invDuration = 1.0f / keyframeDuration;
-	for (int i = clip->firstKeyframeIndex; i <= clip->lastKeyframeIndex; i++)
-	{
-		clip->keyframePool->keyframe[i].duration = keyframeDuration;
-		clip->keyframePool->keyframe[i].inverseDuration = invDuration;
-	}
-
-	return 0;
-}
-
 //-----------------------------------------------------------------------------
