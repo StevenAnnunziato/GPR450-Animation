3c41a20e66ce26a9cf0b8a49cccb0deace73f98b
Steven Annunziato <saannunziato@gmail.com>
2023-02-23 18:23:19 -0500

    Int to string conversions for header information

--- animal3D-SDK/animal3D-SDK/resource/animdata/exporter/export_htr.py
+++ animal3D-SDK/animal3D-SDK/resource/animdata/exporter/export_htr.py
@@ -2,7 +2,7 @@ import bpy
 import struct
 
 # Define the file path and name for the exported HTR file
-file_path = "C:/Users/steven.annunziato/Desktop/Repos/GPR450-Animation/animal3D-SDK/animal3D-SDK/resource/animdata/exporter"
+file_path = "C:/Users/steven.annunziato/Desktop/Repos/GPR450-Animation/animal3D-SDK/animal3D-SDK/resource/animdata/exporter/"
 file_name = "rig_export.htr"
 export_path = file_path + file_name
 
@@ -16,17 +16,18 @@ num_frames = end_frame - start_frame + 1
 frame_rate = bpy
 
 # Open the HTR file for writing
-with open(export_path, "wb") as f:
+with open(export_path, "w") as f:
 
     # Write the header information
     f.write("[Header]\n")
     f.write("# KeyWord<space>Value<CR>")
+
     f.write("FileType htr\n")
     f.write("DataType HTRS\n")
     f.write("FileVersion 1.0\n")
-    f.write("NumSegments " + len(rig_object.data.bones) + "\n")
-    f.write("NumFrames " + num_frames + "\n")
-    f.write("DataFrameRate " + bpy.context.scene.render.fps + "\n")
+    f.write("NumSegments " + str(len(rig_object.data.bones)) + "\n")
+    f.write("NumFrames " + str(num_frames) + "\n")
+    f.write("DataFrameRate " + str(bpy.context.scene.render.fps) + "\n")
     f.write("EulerRotationOrder ZYX\n")
     f.write("CalibrationUnits m\n")
     f.write("RotationUnits Degrees\n")
@@ -35,18 +36,17 @@ with open(export_path, "wb") as f:
     f.write("BoneLengthAxis Z\n")
     f.write("ScaleFactor 1.00\n")
 
-
     # Header information
     f.write("[SegmentNames&Hierarchy]\n")
     f.write("# ObjectName<tab>ParentObjectName<CR>\n")
     
     # Write the bone hierarchy information
     for bone in rig_object.pose.bones:
-        f.write(bone.name.encode("ascii")) # Bone
+        f.write(bone.name) # Bone
         if bone.parent:
-            f.write( bone.parent.name.encode("ascii\n")) # Parent
+            f.write(bone.parent.name + "\n") # Parent
         else:
-            f.write(b"Main\n") # Root
+            f.write("GLOBAL\n") # Root
 
     f.write("[BasePosition]\n")
     f.write("# ObjectName<tab>Tx<tab>Ty<tab>Tz<tab>Rx<tab>Ry<tab>Rz<tab>BoneLength<CR>\n")
@@ -59,11 +59,11 @@ with open(export_path, "wb") as f:
                                                 from_space="POSE", 
                                                 to_space="LOCAL")
         loc = bone_matrix.to_translation()
-        rot = bone_matrix.to_quaternion().to_axis_angle();
+        rot = bone_matrix.to_quaternion().to_axis_angle()
         scale = bone_matrix.to_scale()
-        f.write("\t" +loc.x +"\t"+loc.y+"\t" +"\t"+ loc.z))
-        f.write("\t" +rot.x +"\t"+rot.y+"\t" +"\t"+ rot.z))
-        f.write("\t" +scale.x))
+        f.write("\t" +loc.x +"\t"+loc.y+"\t" +"\t"+ loc.z)
+        f.write("\t" +rot.x +"\t"+rot.y+"\t" +"\t"+ rot.z)
+        f.write("\t" +scale.x)
 
     # Write the animation data for each frame
     for i in range(start_frame, end_frame + 1):
@@ -74,10 +74,10 @@ with open(export_path, "wb") as f:
                                                     from_space="POSE", 
                                                     to_space="LOCAL")
             loc = bone_matrix.to_translation()
-            rot = bone_matrix.to_quaternion().to_axis_angle();
+            rot = bone_matrix.to_quaternion().to_axis_angle()
             scale = bone_matrix.to_scale()
-            f.write("\n"+i + "\t" +loc.x +"\t"+loc.y+"\t" +"\t"+ loc.z))
-            f.write("\t" +rot.x +"\t"+rot.y+"\t" +"\t"+ rot.z))
-            f.write("\t" +scale.x))
+            f.write("\n"+i + "\t" +loc.x +"\t"+loc.y+"\t" +"\t"+ loc.z)
+            f.write("\t" +rot.x +"\t"+rot.y+"\t" +"\t"+ rot.z)
+            f.write("\t" +scale.x)
             
     f.write("\n[EndOfFile]")
